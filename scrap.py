39511929267a4c263baeba6e33de7b17a7571ee9
unknown <530579714@qq.com>
2019-08-16 10:38:57 +0800

    fixed the compatibility problems
    Now the script can be run both on the Win and Lin platforms.Note that the working dir is consistent with current console.

--- scrap.py
+++ scrap.py
@@ -10,15 +10,25 @@ import requests
 import csv
 import argparse
 
+        
+
+
+file_help = "select csv file(指定csv文件),请不要在路径中包含空格。"
+
+
+abs_path = os.path.split(os.path.abspath("__file__"))[0]
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--csv", '-c', dest='filename', help="select csv file(指定csv文件)")
+parser.add_argument("--csv", '-c', dest='filename', help=file_help)
 
 
 csv_arg = parser.parse_args()
 csv_file = csv_arg.filename
 if csv_file == None:
     print('No file input or input failed, default file has been added in.')
-    csv_file = os.path.abspath('.') +'\\starlist.csv'
+    csv_file = os.path.join(abs_path,'starlist.csv')
+    
+scale = 1
 
 
 i = 0
@@ -27,18 +37,30 @@ with open(csv_file, 'r') as file_in:
     f_csv = csv.reader(file_in)
     for obj in f_csv:
         ra, dec = obj[0],obj[1]
-        print(ra, dec)
+        
+        print(str(i)+"    ra: "+ra+"   dec: "+dec)
         width, height = 500, 300
         url = "http://skyserver.sdss.org/dr12/SkyserverWS/ImgCutout/getjpeg?"+ \
                 "ra="+str(ra)+ \
                 "&dec="+str(dec)+ \
                 "&width="+str(width)+ \
                 "&height="+str(height)+ \
-                "&scale=1"
+                "&scale="+str(scale)
                 
         response = requests.get(url)
         
-        filename = os.getcwd()+"\\Download\\{:0>6}.jpg".format(str(i))
-        with open(filename,"wb") as f:
-            f.write(response.content)
+        
+        filename = os.path.join(abs_path, "Download","{:0>6}.jpg".format(str(i)) )
+        with open(filename,"wb") as img:
+            img.write(response.content)
         i = i + 1
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
